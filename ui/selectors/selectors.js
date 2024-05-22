d2bb96062430e3c9fe2b649f769af2f740fc9dca
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-05-22 15:11:01 -0400

    updating the status changes

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -2577,6 +2577,14 @@ export function getMetaMetricsDataDeletionStatus(state) {
   return state.metamask.metaMetricsDataDeletionStatus;
 }
 
+export function isMetaMetricsDataDeletionMarked(state) {
+  return state.appState.metaMetricsDataDeletionMarked;
+}
+
+export function hasRecordedMetricsSinceDeletion(state) {
+  return state.appState.hasRecordedMetricsSinceDeletion;
+}
+
 /**
  * To get all installed snaps with proper metadata
  *
