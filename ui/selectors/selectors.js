d274a67a49989380f6f3b122b11711d7670ec1c5
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-06-13 17:34:15 -0400

    fixing rebase issues

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -2565,10 +2565,6 @@ export function getShowDataDeletionErrorModal(state) {
   return state.appState.showDataDeletionErrorModal;
 }
 
-export function getParticipateInMetaMetrics(state) {
-  return state.metamask.participateInMetaMetrics;
-}
-
 export function getMetaMetricsDataDeletionId(state) {
   return state.metamask.metaMetricsDataDeletionId;
 }
